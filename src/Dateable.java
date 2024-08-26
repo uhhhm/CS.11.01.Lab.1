28ccf9fb989c10e462cb8e8132850171c3eea79c
uhhhm <allenwang513@gmail.com>
2024-08-26 08:20:39 +0800

    a

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = scan.nextInt();
+        System.out.println(age + " years old should be date somebody who is at least " + (age/2+7) + " years old.");
+
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = scan.nextInt();
+        System.out.println(age + " years old should read at least " + (100-age) + " before giving up on a book.");
 
     }
 }
