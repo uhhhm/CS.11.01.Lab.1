c10b2f368f57fb07dc32792c9e8341ee0312ef97
uhhhm <allenwang513@gmail.com>
2024-08-28 09:50:53 +0800

    a

--- src/Pages.java
+++ src/Pages.java
@@ -5,7 +5,18 @@ public class Pages {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter your age: ");
-        int age = scan.nextInt();
+        int age = -1;
+        while(age < 0) {
+            try {
+                age = Integer.parseInt(scan.nextLine());
+                if(age < 0){
+                    System.out.println("Please enter a valid number!");
+                }
+            }
+            catch(NumberFormatException e){
+                System.out.println("Please enter a valid number!");
+            }
+        }
         System.out.println(age + " years old should read at least " + (100-age) + " before giving up on a book.");
 
     }
